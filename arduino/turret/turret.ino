c7acce17df38afe2cef61c5929d591cff0841a45
CuttrellW <wcuttrell@gmail.com>
2024-09-15 20:35:35 -0600

    update turret.ino to use 270 degree servos

--- arduino/turret/turret.ino
+++ arduino/turret/turret.ino
@@ -9,18 +9,26 @@ WiFiServer server(80);
 Servo xServo;
 Servo yServo;
 
-int xPos = 90;
-int yPos = 90;
-const int stepSize = 5;
+// Initial positions (degrees)
+int xPos = 135;  // Center position for 270-degree servo
+int yPos = 0;
+
+// Servo pulse width range (microseconds)
+int minPulseWidth = 500;   // Adjust according to your servo's datasheet
+int maxPulseWidth = 2500;  // Adjust according to your servo's datasheet
 
 void setup() {
   Serial.begin(115200);
 
-  xServo.attach(8);
-  yServo.attach(9);
+  // Attach servos with specified pulse width range
+  xServo.attach(8, minPulseWidth, maxPulseWidth);
+  yServo.attach(9, minPulseWidth, maxPulseWidth);
+
+  // Move servos to initial positions
   xServo.write(xPos);
   yServo.write(yPos);
 
+  // Connect to Wi-Fi network
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -40,29 +48,79 @@ void loop() {
   if (client) {
     Serial.println("New Client Connected");
 
+    String request = "";
+
     while (client.connected()) {
       if (client.available()) {
-        char received = client.read();
-        Serial.print("Received: ");
-        Serial.println(received);
+        char c = client.read();
+        if (c == '\n') {
+          // End of request
+          request.trim();  // Remove any leading/trailing whitespace
+          Serial.print("Received: ");
+          Serial.println(request);
 
-        if (received == 'w') {
-          yPos = constrain(yPos - stepSize, 0, 180);
-          yServo.write(yPos);
-        } else if (received == 's') {
-          yPos = constrain(yPos + stepSize, 0, 180);
-          yServo.write(yPos);
-        } else if (received == 'a') {
-          xPos = constrain(xPos - stepSize, 0, 180);
-          xServo.write(xPos);
-        } else if (received == 'd') {
-          xPos = constrain(xPos + stepSize, 0, 180);
-          xServo.write(xPos);
+          // Parse positions from the request
+          parseAndSetPositions(request, client);
+
+          // Clear the request string for the next message
+          request = "";
+        } else {
+          request += c;
         }
       }
     }
 
+    int xPulseWidth = angleToPulseWidth(135);
+    int yPulseWidth = angleToPulseWidth(0);
+
+    // Move servos to the specified positions
+    xServo.writeMicroseconds(xPulseWidth);
+    yServo.writeMicroseconds(yPulseWidth);
+
     client.stop();
     Serial.println("Client Disconnected");
+  }
+}
+
+int angleToPulseWidth(int angle) {
+  return map(angle, 0, 270, minPulseWidth, maxPulseWidth);
+}
+
+void parseAndSetPositions(String request, WiFiClient& client) {
+  // Expected format: "x=<value>&y=<value>"
+  int xIndex = request.indexOf("x=");
+  int yIndex = request.indexOf("y=");
+  
+
+  if (xIndex != -1 && yIndex != -1) {
+    int xEndIndex = request.indexOf('&', xIndex);
+    String xValueStr = request.substring(xIndex + 2, xEndIndex);
+    String yValueStr = request.substring(yIndex + 2);
+
+    int xValue = xValueStr.toInt();
+    int yValue = yValueStr.toInt();
+
+    // Constrain values to servo limits
+    xPos = constrain(xValue, 0, 270);
+    yPos = constrain(yValue, 0, 270);
+
+    // Calculate pulse widths
+    int xPulseWidth = angleToPulseWidth(xPos);
+    int yPulseWidth = angleToPulseWidth(yPos);
+
+    // Move servos to the specified positions
+    xServo.writeMicroseconds(xPulseWidth);
+    yServo.writeMicroseconds(yPulseWidth);
+
+    Serial.print("Moving xServo to ");
+    Serial.print(xPos);
+    Serial.print(" degrees, yServo to ");
+    Serial.print(yPos);
+    Serial.println(" degrees");
+
+    // Send response to client
+    client.println("Servos moved to positions: x=" + String(xPos) + ", y=" + String(yPos));
+  } else {
+    client.println("Invalid command format. Expected: x=<value>&y=<value>");
   }
 }
